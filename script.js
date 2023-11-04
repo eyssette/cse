6b16306f04d4ddf335032e18e5d251a604289d4e
eyssette <cedric.eyssette@gmail.com>
2023-11-04 10:09:45 +0100

    Ajout d'un exemple de CSE

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let md = `# Créez votre moteur de recherche
 
 > Ce site vous permet de créer un moteur de recherche personnalisé à partir d'une liste de sites.
 > 1. Créez un fichier sur CodiMD ou sur une forge
-> 2. Ce fichier doit comporter : un titre, un bloc de citation qui constituera le message initial, et une liste de sites. Vous pouvez récupérer [ce modèle](https://codimd.apps.education.fr/b8KAltV2QQWR2rKhF_eYcg?both).
+> 2. Ce fichier doit comporter : un titre, un bloc de citation qui constituera le message initial, et une liste de sites. Vous pouvez récupérer [ce modèle](https://codimd.apps.education.fr/b8KAltV2QQWR2rKhF_eYcg?both) ou bien regarder cet [exemple](https://eyssette.forge.aeif.fr/cse#https://eyssette.forge.aeif.fr/my-cse/intro-philo.md).
 > 3. Votre moteur de recherche sera alors disponible à l'adresse : https://eyssette.forge.aeif.fr/cse#URL (en remplaçant URL par l'URL de votre fichier)
 
 - eyssette.github.io`;
