2620c22677673dd715da2bce831c4bd32fb5cd65
Eyssette <cedric.eyssette@gmail.com>
2024-08-17 17:04:09 +0200

    URLs : sur la forge apps

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ const cseDefault = `# Créez votre moteur de recherche
 
 > Ce site vous permet de créer un moteur de recherche personnalisé à partir d'une liste de sites.
 > 1. Créez un fichier sur CodiMD ou sur une forge.
-> 2. Ce fichier doit comporter : un titre, un bloc de citation qui constituera le message initial, et une liste de sites. Vous pouvez récupérer [ce modèle](https://codimd.apps.education.fr/b8KAltV2QQWR2rKhF_eYcg?both) ou bien regarder cet [exemple](https://eyssette.forge.aeif.fr/cse#https://eyssette.forge.aeif.fr/my-cse/intro-philo.md).
+> 2. Ce fichier doit comporter : un titre, un bloc de citation qui constituera le message initial, et une liste de sites. Vous pouvez récupérer [ce modèle](https://codimd.apps.education.fr/b8KAltV2QQWR2rKhF_eYcg?both) ou bien regarder cet [exemple](https://cse.forge.apps.education.fr/#https://eyssette.forge.apps.education.fr/my-cse/intro-philo.md).
 > 3. La recherche est plus pertinente si votre liste de sites n'est pas trop longue. Vous pouvez également utiliser le caractère générique \`*\`. Par exemple : \`ac-*.fr\` permet d'intégrer tous les sites \`ac-lyon.fr\`, \`ac-grenoble.fr\`, etc.
-> 4. Votre moteur de recherche sera alors disponible à l'adresse : https://eyssette.forge.aeif.fr/cse#URL (en remplaçant URL par l'URL de votre fichier).
+> 4. Votre moteur de recherche sera alors disponible à l'adresse : https://cse.forge.apps.education.fr/#URL (en remplaçant URL par l'URL de votre fichier).
 > 5. Si votre fichier n'est pas accessible : essayez de mettre \`https://api.allorigins.win/raw?url=\` avant votre URL
 
 - eyssette.github.io`;
